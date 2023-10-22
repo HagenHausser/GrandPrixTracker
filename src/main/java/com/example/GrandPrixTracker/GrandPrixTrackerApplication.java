31e59416549bd0cd6cd307f74adb242d0f57be0d
Hagen Hausser <Hagen.Hausser@Student.HTW-Berlin.de>
2023-10-22 16:21:09 +0200

    Adding to GitHub

---
+++ src/main/java/com/example/GrandPrixTracker/GrandPrixTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.GrandPrixTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandPrixTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandPrixTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GrandPrixTracker/GrandPrixTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GrandPrixTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandPrixTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
